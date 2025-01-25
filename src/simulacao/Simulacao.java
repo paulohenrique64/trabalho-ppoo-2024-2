594d013eefb3fecab4950b99125f1442c618ff88
kaue-oliveira <kaueolivsil@gmail.com>
2025-01-25 15:38:26 -0300

    Atualizacao do arquivo Simulacao

--- src/simulacao/Simulacao.java
+++ src/simulacao/Simulacao.java
@@ -1,7 +1,6 @@
 package simulacao;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -14,72 +13,63 @@ import veiculos.Moto;
 import veiculos.Veiculo;
 
 public class Simulacao {
-    private JanelaSimulacao janelaSimulacao; // interface grafica
-    private Estacionamento estacionamento; // logica do sistema
+  private JanelaSimulacao janelaSimulacao;
+  private Estacionamento estacionamento;
 
-    public Simulacao() {
-        estacionamento = new Estacionamento();
-        janelaSimulacao = new JanelaSimulacao(estacionamento);
-    }
+  public Simulacao() {
+    estacionamento = new Estacionamento();
+    janelaSimulacao = new JanelaSimulacao(estacionamento);
+  }
 
-    public void iniciarSimulacao() {
-        List<Veiculo> veiculosRodando = new ArrayList<>();
-        veiculosRodando.add(new Moto(new Localizacao(10, 10), 4));
-        veiculosRodando.add(new Carro(new Localizacao(10, 10), 4));
-        
-       
+  public void iniciarSimulacao() {
+    List<Veiculo> veiculos = new ArrayList<>();
+    veiculos.add(new Moto(new Localizacao(10, 10), 4));
+    veiculos.add(new Moto(new Localizacao(10, 10), 4));
+    veiculos.add(new Carro(new Localizacao(10, 10), 4));
+    veiculos.add(new Carro(new Localizacao(10, 10), 4));
 
-        for (int i = 0; i < veiculosRodando.size(); i++) {
-            final int index = i;
 
-            
-            Thread teste = new Thread(new Runnable() {
-               
-                @Override
-                public void run() {
-                    System.out.println(" thread do " + veiculosRodando.get(index).getClass().getName());
-                    estacionamento.estacionarVeiculo(veiculosRodando.get(index));
-
-                    try (BufferedReader leitor = new BufferedReader(
-                            new FileReader("data/vaga-estacionamento-"+(index + 1)+"-caminho.txt"))) {
-                        String linha;
-                        int x;
-                        int y;
-
-                        while ((linha = leitor.readLine()) != null) {
-                            String[] coordenasString = linha.split(" ");
-                            coordenasString = coordenasString[1].split("\"");
-                            coordenasString = coordenasString[1].split(";");
-
-                            x = Integer.parseInt(coordenasString[0]);
-                            y = Integer.parseInt(coordenasString[1]);
-
-                            // System.out.println("colocando a " + veiculosRodando.get(index).getClass().getName() + " na coordenada " + x + " " + y);
-
-                            // System.out.println(x + " " + y);
-
-                            estacionamento.removerItem(veiculosRodando.get(index));
-                            veiculosRodando.get(index).setLocalizacaoAtual(new Localizacao(y, x));
-                            estacionamento.adicionarItem(veiculosRodando.get(index));
-
-                            janelaSimulacao.atualizarJanelaSimulacao();
-                            try {
-                                Thread.sleep(10);
-                            } catch (InterruptedException e) {
-                                e.printStackTrace();
-                            }
-                        }
-                    } catch (IOException e) {
-                        System.err.println("Erro ao ler o arquivo: " + e.getMessage());
-                    }
-
-                    // após o tempo aleatório o carro desaparece do mapa e deixa a vaga disponível
-                    estacionamento.desestacionarVeiculo(veiculosRodando.get(index));
-                }
-            });
-
-            // Agora você deve iniciar o thread
-            teste.start();
+    for (int i = 0; i < veiculos.size(); i++) {
+      final int numeroVeiculo = i;
+      Thread thread = new Thread(new Runnable() {
+        @Override
+        public void run() {
+          moverVeiculo(veiculos.get(numeroVeiculo), numeroVeiculo + 1);
         }
+      });
+      thread.start();
     }
+  }
+
+  private void moverVeiculo(Veiculo veiculo, int numero) {
+    estacionamento.estacionarVeiculo(veiculo);
+    String arquivo = "data/vaga-estacionamento-" + numero + "-caminho.txt";
+
+    try (BufferedReader leitor = new BufferedReader(new FileReader(arquivo))) {
+      String linha;
+      while ((linha = leitor.readLine()) != null) {
+        moverParaNovaPosicao(veiculo, linha);
+        Thread.sleep(70);
+      }
+    } catch (IOException | InterruptedException e) {
+      e.printStackTrace();
+    }
+
+    estacionamento.desestacionarVeiculo(veiculo);
+  }
+
+  
+
+  private void moverParaNovaPosicao(Veiculo veiculo, String linha) {
+    String[] coordenadas = linha.split(" ")[1].split("\"")[1].split(";");
+    int x = Integer.parseInt(coordenadas[0]);
+    int y = Integer.parseInt(coordenadas[1]);
+
+    synchronized (estacionamento) {
+      estacionamento.removerItem(veiculo);
+      veiculo.setLocalizacaoAtual(new Localizacao(y, x));
+      estacionamento.adicionarItem(veiculo);
+      janelaSimulacao.atualizarJanelaSimulacao();
+    }
+  }
 }
